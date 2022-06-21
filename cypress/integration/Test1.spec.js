da8a7a9380ee3ea46e70343bc2a3f1164404e5c1
Fazi777 <golaszewski8@gmail.com>
2022-06-21 17:24:28 +0200

    2

--- cypress/integration/Test1.spec.js
+++ cypress/integration/Test1.spec.js
@@ -59,5 +59,62 @@ context('Testy Input BMI', () => {
             cy.get('#wynikBMI').should('not.be.visible')
         })
     })
-    
+    describe('Test ładowania wagi z zapisu historii', () => {
+        it('Test 7', () => {
+            cy.visit('https://fazi777.github.io/BMI-Host/');
+            cy.url().should('contain', 'BMI');
+            cy.get('#podajWage').type('80');
+            cy.get('#podajWzrost').type('180')
+            cy.get('#calculate').click();
+            cy.get('#podajWage').type('60');
+            cy.get('#podajWzrost').type('120')
+            cy.get('#calculate').click()
+            cy.get('#zapisyhistorii > :nth-child(1)').click()
+            cy.get('#statusWagi').should('contain','80')
+            
+        })
+    })
+    describe('Test ładowania wzrostu z zapisu historii', () => {
+        it('Test 8', () => {
+            cy.visit('https://fazi777.github.io/BMI-Host/');
+            cy.url().should('contain', 'BMI');
+            cy.get('#podajWage').type('80');
+            cy.get('#podajWzrost').type('180')
+            cy.get('#calculate').click();
+            cy.get('#podajWage').type('60');
+            cy.get('#podajWzrost').type('120')
+            cy.get('#calculate').click()
+            cy.get('#zapisyhistorii > :nth-child(1)').click()
+            cy.get('#statusWzrostu').should('contain','180')
+            
+        })
+    })
+    describe('Test wskazania wzrostu BMI', () => {
+        it('Test 9', () => {
+            cy.visit('https://fazi777.github.io/BMI-Host/');
+            cy.url().should('contain', 'BMI');
+            cy.get('#podajWage').type('80');
+            cy.get('#podajWzrost').type('180')
+            cy.get('#calculate').click();
+            cy.get('#podajWage').type('60');
+            cy.get('#podajWzrost').type('120')
+            cy.get('#calculate').click()
+            cy.get('#porownanieBMI').should('contain','wzrosło')
+            
+        })
+    })
+    describe('Test wskazania spadku BMI', () => {
+        it('Test 10', () => {
+            cy.visit('https://fazi777.github.io/BMI-Host/');
+            cy.url().should('contain', 'BMI');
+            cy.get('#podajWage').type('80');
+            cy.get('#podajWzrost').type('180')
+            cy.get('#calculate').click();
+            cy.get('#podajWage').type('60');
+            cy.get('#podajWzrost').type('180')
+            cy.get('#calculate').click()
+            cy.get('#porownanieBMI').should('contain','Spadło')
+            
+        })
+    })
 })
